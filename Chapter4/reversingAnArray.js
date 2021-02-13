fcb621a68e1188f594c6b67c73765b43c11c77c1
ElMagdo <magdoe19@gmail.com>
2021-02-13 23:52:20 +0100

    Task completed!!

--- Chapter4/reversingAnArray.js
+++ Chapter4/reversingAnArray.js
@@ -10,5 +10,28 @@ function reverseArray(arr) {
 console.log(reverseArray([1, 2, 3, 4, 5]));
 
 function reverseArrayInPlace(arr) {
+    let i = 0;
+    let j = Math.ceil(arr.length / 2);
+    let k = arr.length - 1;
+    let tmp;
 
+    for (i = 0; i < j; i++) {
+        tmp = arr[i];
+        arr[i] = arr[k - i];
+        arr[k - i] = tmp;
+    }
+    
+    /*Alternative using other loop
+    for (let num of arr) {
+        if (i >= j) break;
+
+        arr[i] = arr[k - i];
+        arr[k - i] = num;
+        i++;
+        //k--;
+    }*/
+
+    return arr;
 }
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6]));
