f753f9efb98d218eb294af1383295ab612702cbc
ElMagdo <magdoe19@gmail.com>
2021-02-17 11:44:56 +0100

    Arranged the array.

--- Chapter5/flattening.js
+++ Chapter5/flattening.js
@@ -2,6 +2,6 @@ function flatten(arr) {
     return arr.reduce((a, b) => a.concat(b));
 }
 
-let num = [[1, 2, 3], [3, 4, 5], [5, 6, 7]];
+let num = [[1, 2, 3], [4, 5], [6]];
 let tmp = flatten(num);
 console.log(tmp);
