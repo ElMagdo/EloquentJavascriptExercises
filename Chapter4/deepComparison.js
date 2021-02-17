9ffbe4b8249bd1aeab4e809bc9b9b4f2c426be59
ElMagdo <magdoe19@gmail.com>
2021-02-17 11:42:58 +0100

    Fixed a bug. Works good for tight conditions.

--- Chapter4/deepComparison.js
+++ Chapter4/deepComparison.js
@@ -5,21 +5,28 @@ function deepEqual(val1, val2) {
     if (v1 && v2) {
         var arr1 = Object.keys(val1), arr2 = Object.keys(val2);
         //console.log(arr1.length + " " + arr2.length);
+        //console.log(arr1, arr2)
         
         if (arr1.length == arr2.length) {
             for (let i of arr1) {
                 //console.log(val1[i] + " " + val2[i]);
-                if (val1[i] != val2[i]) return false;
+                if (!deepEqual(val1[i], val2[i])) return false;
             }
             return true;
-        } else return false;
-    } else return val1 === val2;
+        }
+        return false;
+    }
+    return val1 === val2;
 }
 
-var obj1 = {val: 3, res: "true"}, obj2, obj3 = {val: 3, res: "true"};
-obj2 = obj1;
+var obj1 = {here: {is: "an"}, object: 2};
+var obj2 = {here: {is: "an"}, object: 2}, obj3 = obj1;
+
+console.log(deepEqual(obj1, obj1));
+console.log(deepEqual(obj1, {here: 1, object: 2}));
 console.log(deepEqual(obj1, obj2));
 console.log(deepEqual(obj1, obj3));
+console.log(deepEqual(obj1, {here: {is: "an"}, object: 2}));
 console.log(deepEqual("2", 2));
-console.log(deepEqual('2', obj2));
+console.log(deepEqual('2', obj1));
 console.log(deepEqual(1, 1));
