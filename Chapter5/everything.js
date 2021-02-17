b3083d2aed8177d7990d0c63ac9860fbc27e6087
ElMagdo <magdoe19@gmail.com>
2021-02-17 11:55:22 +0100

    Used a different function.

--- Chapter5/everything.js
+++ Chapter5/everything.js
@@ -5,12 +5,20 @@ function everyForLoop(arr, func) {
     return true;
 }
 
-let num = [2, 4, 6, 8, 32];
-console.log(everyForLoop(num, (a) => a%2 == 0));
+//let num = [2, 4, 6, 8, 32];
+//console.log(everyForLoop(num, (a) => a%2 == 0));
+console.log("Using loop");
+console.log(everyForLoop([1, 3, 5], n => n < 10));
+console.log(everyForLoop([2, 4, 16], n => n < 10));
+console.log(everyForLoop([], n => n < 10));
 
 function everySomeMethod(arr, func) {
     if (arr.some((a) => func(a))) return false;
     return true;
 }
 
-console.log(everySomeMethod(num, (b) => b%2 == 1));
+//console.log(everySomeMethod(num, (b) => b%2 == 1));
+console.log("Using some method");
+console.log(everyForLoop([1, 3, 5], n => n < 10));
+console.log(everyForLoop([2, 4, 16], n => n < 10));
+console.log(everyForLoop([], n => n < 10));
