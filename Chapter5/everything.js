9fc3414c06e148a22b2af3532d3ddd0cad4cd874
ElMagdo <magdoe19@gmail.com>
2021-02-17 08:23:42 +0100

    Finished second part. Task completed.

--- Chapter5/everything.js
+++ Chapter5/everything.js
@@ -5,5 +5,12 @@ function everyForLoop(arr, func) {
     return true;
 }
 
-let num = [2, 4, 6, 8];
+let num = [2, 4, 6, 8, 32];
 console.log(everyForLoop(num, (a) => a%2 == 0));
+
+function everySomeMethod(arr, func) {
+    if (arr.some((a) => func(a))) return false;
+    return true;
+}
+
+console.log(everySomeMethod(num, (b) => b%2 == 1));
