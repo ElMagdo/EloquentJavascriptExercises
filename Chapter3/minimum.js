fb86a2ef920902d5d0118e7bcbb544b49f28c613
ElMagdo <magdoe19@gmail.com>
2021-02-17 10:31:05 +0100

    Added more testing conditions.

--- Chapter3/minimum.js
+++ Chapter3/minimum.js
@@ -1,6 +1,8 @@
 function myMin(num1, num2) {
     if (num1 <= num2) return num1;
-    else return num2;
+    return num2;
 }
 
 console.log(myMin(4, 5));
+console.log(myMin(0, 10));
+console.log(myMin(0, -10));
