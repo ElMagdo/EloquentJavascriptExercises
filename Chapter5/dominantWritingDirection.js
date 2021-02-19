e48d8e9f2ed99646c518e5df816757d11e382bdb
ElMagdo <magdoe19@gmail.com>
2021-02-19 09:09:49 +0100

    Added second function for subtask.

--- Chapter5/dominantWritingDirection.js
+++ Chapter5/dominantWritingDirection.js
@@ -8,3 +8,17 @@ return script;
 }
 return null;
 }
+
+function countBy(items, groupName) {
+let counts = [];
+for (let item of items) {
+let name = groupName(item);
+let known = counts.findIndex(c => c.name == name);
+if (known == -1) {
+counts.push({name, count: 1});
+} else {
+counts[known].count++;
+}
+}
+return counts;
+}
