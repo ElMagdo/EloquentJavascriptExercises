fb03176e9801ff71b1c0c8d05dd433ee0b4fe40d
ElMagdo <magdoe19@gmail.com>
2021-02-19 09:10:51 +0100

    Added last function for subtask, ready testing.

--- Chapter5/dominantWritingDirection.js
+++ Chapter5/dominantWritingDirection.js
@@ -22,3 +22,16 @@ counts[known].count++;
 }
 return counts;
 }
+
+function textDirection(text) {
+let scripts = countBy(text, char => {
+let script = characterScript(char.codePointAt(0));
+return script ? script.direction : "none";
+}).filter(({name}) => name != "none");
+let total = scripts.reduce((n, {count}) => n + count, 0);
+if (total == 0) return "No scripts found";
+return scripts.map(({name, count}) => {
+return `${Math.round(count * 100 / total)}% ${name}`;
+}).join(", ");
+}
+console.log(textDirection(' 英国的狗说 "woof",俄罗斯的狗说 "тяв"'));
