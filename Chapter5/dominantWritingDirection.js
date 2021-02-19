7895e525fe04b246068ecef1ff4fcbae05fa6624
ElMagdo <magdoe19@gmail.com>
2021-02-19 09:17:43 +0100

    Task is 95% completed, but lacking SCRIPTS file.

--- Chapter5/dominantWritingDirection.js
+++ Chapter5/dominantWritingDirection.js
@@ -1,37 +1,37 @@
 function characterScript(code) {
-for (let script of SCRIPTS) {
-if (script.ranges.some(([from, to]) => {
-return code >= from && code < to;
-})) {
-return script;
-}
-}
-return null;
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+                return code >= from && code < to;
+            })) {
+            return script;
+        }
+    }
+    return null;
 }
 
 function countBy(items, groupName) {
-let counts = [];
-for (let item of items) {
-let name = groupName(item);
-let known = counts.findIndex(c => c.name == name);
-if (known == -1) {
-counts.push({name, count: 1});
-} else {
-counts[known].count++;
-}
-}
-return counts;
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+            counts.push({name, count: 1});
+        } else {
+            counts[known].count++;
+        }
+    }
+    return counts;
 }
 
 function textDirection(text) {
-let scripts = countBy(text, char => {
-let script = characterScript(char.codePointAt(0));
-return script ? script.direction : "none";
-}).filter(({name}) => name != "none");
-let total = scripts.reduce((n, {count}) => n + count, 0);
-if (total == 0) return "No scripts found";
-return scripts.map(({name, count}) => {
-return `${Math.round(count * 100 / total)}% ${name}`;
-}).join(", ");
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.direction : "none";
+    }).filter(({name}) => name != "none");
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found";
+    return scripts.map(({name, count}) => {
+        return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
 }
 console.log(textDirection(' 英国的狗说 "woof",俄罗斯的狗说 "тяв"'));
