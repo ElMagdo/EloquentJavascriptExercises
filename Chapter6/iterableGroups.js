9efc9cd2e84f2fddefa871282f87e4b8a78a2efc
ElMagdo <magdoe19@gmail.com>
2021-02-24 15:28:41 +0100

    Completed 50% of sub-task.

---
+++ Chapter6/iterableGroups.js
@@ -0,0 +1,48 @@
+class Group {
+
+    constructor() {
+        this.member = [];
+    }
+
+    add(val) {
+        if (this.member.indexOf(val) == -1) this.member.push(val);
+    }
+
+    delete(val) {
+        this.member = this.member.filter(i => i != val);
+    }
+
+    has(val) {
+        return this.member.includes(val);
+    }
+
+    static from(arr) {
+        let a = new Group();
+        for (let i of arr) a.member.push(i);
+        return a;
+    }
+}
+
+class GroupIterator {
+    constructor(group) {
+        this.i = 0;
+        this.member = group;
+    }
+
+    next() {
+        if (this.i = this.member.length - 1) return {done: true};
+
+        let value = {value: this.member.at(i)};
+        this.i++;
+
+        return {value, done: false};
+    }
+}
+
+Group.prototype[Symbol.iterator] = function() {
+    return new GroupIterator(this);
+}
+
+for (let value of Group.from(["a", "b", "c"])) {
+    console.log(value);
+}
