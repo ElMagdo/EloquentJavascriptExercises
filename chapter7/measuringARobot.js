856ee43134bb9504378735e2e73ae0bf8dfab9d5
ElMagdo <magdoe19@gmail.com>
2021-02-27 18:57:32 +0100

    Finished final editing of sub task.

--- chapter7/measuringARobot.js
+++ chapter7/measuringARobot.js
@@ -6,7 +6,6 @@ const roads = [
 "Grete's House-Shop",
 "Marketplace-Post Office",
 "Marketplace-Town Hall",
-
 "Alice's House-Cabin",
 "Bob's House-Town Hall",
 "Daria's House-Town Hall",
@@ -17,75 +16,77 @@ const roads = [
 ];
 
 function buildGraph(edges) {
-let graph = Object.create(null);
-function addEdge(from, to) {
-if (graph[from] == null) {
-graph[from] = [to];
-} else {
-graph[from].push(to);
-}
-}
-for (let [from, to] of edges.map(r => r.split("-"))) {
-addEdge(from, to);
-addEdge(to, from);
-}
-return graph;
+    let graph = Object.create(null);
+
+    function addEdge(from, to) {
+        if (graph[from] == null) {
+            graph[from] = [to];
+        } else {
+            graph[from].push(to);
+        }
+    }
+    for (let [from, to] of edges.map(r => r.split("-"))) {
+        addEdge(from, to);
+        addEdge(to, from);
+    }
+    return graph;
 }
 
 const roadGraph = buildGraph(roads);
-//console.log(roadGraph);
 
 class VillageState {
-constructor(place, parcels) {
-this.place = place;
-this.parcels = parcels;
-}
-move(destination) {
-if (!roadGraph[this.place].includes(destination)) {
-return this;
-} else {
-let parcels = this.parcels.map(p => {
-if (p.place != this.place) return p;
-return {place: destination, address: p.address};
-}).filter(p => p.place != p.address);
-return new VillageState(destination, parcels);
-}
-}
+    constructor(place, parcels) {
+        this.place = place;
+        this.parcels = parcels;
+    }
+
+    move(destination) {
+        if (!roadGraph[this.place].includes(destination)) {
+            return this;
+        } else {
+            let parcels = this.parcels.map(p => {
+                if (p.place != this.place) return p;
+                return {place: destination, address: p.address};
+            }).filter(p => p.place != p.address);
+            return new VillageState(destination, parcels);
+        }
+    }
 }
 
 function randomPick(array) {
-let choice = Math.floor(Math.random() * array.length);
-return array[choice];
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
 }
+
 function randomRobot(state) {
-return {direction: randomPick(roadGraph[state.place])};
+    return {direction: randomPick(roadGraph[state.place])};
 }
 
 VillageState.random = function(parcelCount = 5) {
-let parcels = [];
-for (let i = 0; i < parcelCount; i++) {
-let address = randomPick(Object.keys(roadGraph));
-let place;
-do {
-place = randomPick(Object.keys(roadGraph));
-} while (place == address);
-parcels.push({place, address});
-}
-return new VillageState("Post Office", parcels);
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = randomPick(Object.keys(roadGraph));
+        } while (place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState("Post Office", parcels);
 };
 
 const mailRoute = [
-"Alice's House", "Cabin", "Alice's House", "Bob's House",
-"Town Hall", "Daria's House", "Ernie's House",
-"Grete's House", "Shop", "Grete's House", "Farm",
-"Marketplace", "Post Office"
+    "Alice's House", "Cabin", "Alice's House", "Bob's House",
+    "Town Hall", "Daria's House", "Ernie's House",
+    "Grete's House", "Shop", "Grete's House", "Farm",
+    "Marketplace", "Post Office"
 ];
 
 function routeRobot(state, memory) {
-if (memory.length == 0) {
-memory = mailRoute;
-}
-return {direction: memory[0], memory: memory.slice(1)};
+    if (memory.length == 0) {
+        memory = mailRoute;
+    }
+    return {direction: memory[0], memory: memory.slice(1)};
 }
 
 function runRobot(state, robot, memory) {
@@ -99,28 +100,28 @@ function runRobot(state, robot, memory) {
 }
 
 function findRoute(graph, from, to) {
-let work = [{at: from, route: []}];
-for (let i = 0; i < work.length; i++) {
-let {at, route} = work[i];
-for (let place of graph[at]) {
-if (place == to) return route.concat(place);
-if (!work.some(w => w.at == place)) {
-work.push({at: place, route: route.concat(place)});
-}
-}
-}
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i];
+        for (let place of graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)) {
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
 }
 
 function goalOrientedRobot({place, parcels}, route) {
-if (route.length == 0) {
-let parcel = parcels[0];
-if (parcel.place != place) {
-route = findRoute(roadGraph, place, parcel.place);
-} else {
-route = findRoute(roadGraph, place, parcel.address);
-}
-}
-return {direction: route[0], memory: route.slice(1)};
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
 }
 
 function compareRobots(r1, m1, r2, m2) {
