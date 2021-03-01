d17849ff6a7898bf046f859305f75466e39c8720
ElMagdo <magdoe19@gmail.com>
2021-03-01 21:41:19 +0100

    Added the buildGraph function.

--- chapter7/robotEfficiency.js
+++ chapter7/robotEfficiency.js
@@ -15,6 +15,23 @@ const roads = [
 "Shop-Town Hall"
 ];
 
+function buildGraph(edges) {
+    let graph = Object.create(null);
+
+    function addEdge(from, to) {
+        if (graph[from] == null) {
+            graph[from] = [to];
+        } else {
+            graph[from].push(to);
+        }
+    }
+    for (let [from, to] of edges.map(r => r.split("-"))) {
+        addEdge(from, to);
+        addEdge(to, from);
+    }
+    return graph;
+}
+
 function optimalBot({place, parcels}, route) {
   return ({direction: route[0], memory: route.slice(1)});
 }
