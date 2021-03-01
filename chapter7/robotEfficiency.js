7c4b5c4cf26ea92ed8bea367215e532a9277f891
ElMagdo <magdoe19@gmail.com>
2021-03-01 21:44:16 +0100

    Added the village state class.

--- chapter7/robotEfficiency.js
+++ chapter7/robotEfficiency.js
@@ -37,3 +37,22 @@ function optimalBot({place, parcels}, route) {
 }
 
 const roadGraph = buildGraph(roads);
+
+class VillageState {
+    constructor(place, parcels) {
+        this.place = place;
+        this.parcels = parcels;
+    }
+
+    move(destination) {
+        if (!roadGraph[this.place].includes(destination)) {
+            return this;
+        } else {
+            let parcels = this.parcels.map(p => {
+                if (p.place != this.place) return p;
+                return {place: destination, address: p.address};
+            }).filter(p => p.place != p.address);
+            return new VillageState(destination, parcels);
+        }
+    }
+}
