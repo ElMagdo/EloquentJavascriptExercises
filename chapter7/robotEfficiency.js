ad295d18282b2f33f94df0d55564c24945e587d2
ElMagdo <magdoe19@gmail.com>
2021-03-01 19:13:28 +0100

    Added the roads array to be used for routing.

--- chapter7/robotEfficiency.js
+++ chapter7/robotEfficiency.js
@@ -1,3 +1,20 @@
+const roads = [
+"Alice's House-Bob's House",
+"Alice's House-Post Office",
+"Daria's House-Ernie's House",
+"Ernie's House-Grete's House",
+"Grete's House-Shop",
+"Marketplace-Post Office",
+"Marketplace-Town Hall",
+"Alice's House-Cabin",
+"Bob's House-Town Hall",
+"Daria's House-Town Hall",
+"Grete's House-Farm",
+"Marketplace-Farm",
+"Marketplace-Shop",
+"Shop-Town Hall"
+];
+
 function optimalBot({place, parcels}, route) {
   return ({direction: route[0], memory: route.slice(1)});
 }
